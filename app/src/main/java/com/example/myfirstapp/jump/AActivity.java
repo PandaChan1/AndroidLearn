79dacbc1a1110687ee2cba2d9af7bac9cc3d59b7
PandaChan1 <chenyang.74463@gmail.com>
2020-06-02 17:53:13 +0800

    6.2

--- app/src/main/java/com/example/myfirstapp/jump/AActivity.java
+++ app/src/main/java/com/example/myfirstapp/jump/AActivity.java
@@ -2,7 +2,12 @@ package com.example.myfirstapp.jump;
 
 import android.content.ComponentName;
 import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.content.pm.PackageManager;
+import android.icu.text.IDNA;
 import android.os.Bundle;
+import android.service.voice.VoiceInteractionSession;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -14,11 +19,23 @@ import com.example.myfirstapp.R;
 
 public class AActivity extends AppCompatActivity {
     private Button mBtnjumptob;
+    private Button mBtnjumpaself;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_a);
+        Log.d("AActivity","---onCreate---");
+        Log.d("AActivity","taskid:"+getTaskId()+"   hash:"+hashCode());
+        logTaskname();
         mBtnjumptob=findViewById(R.id.btn_jumptob);
+        mBtnjumpaself=findViewById(R.id.btn_jump_aself);
+        mBtnjumpaself.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(AActivity.this,AActivity.class);
+                startActivity(intent);
+            }
+        });
         mBtnjumptob.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -28,8 +45,8 @@ public class AActivity extends AppCompatActivity {
                 bundle.putString("name","PandaChan1");
                 bundle.putInt("number",233);
                 intent.putExtras(bundle);
-//                startActivity(intent);
-                startActivityForResult(intent,0);  //0是为了区分，不止一个按钮调用startActivityForResult，分辨返回的数据属于谁
+                startActivity(intent);
+//                startActivityForResult(intent,0);  //0是为了区分，不止一个按钮调用startActivityForResult，分辨返回的数据属于谁
                 //显式跳转2
 //                Intent intent=new Intent();
 //                intent.setClass(AActivity.this,BActivity.class);
@@ -55,5 +72,22 @@ public class AActivity extends AppCompatActivity {
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         Toast.makeText(AActivity.this,data.getExtras().getString("mesg"),Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    protected void onNewIntent(Intent intent) {
+        super.onNewIntent(intent);
+        Log.d("AActivity","---onNewIntent---");
+        Log.d("AActivity","taskid:"+getTaskId()+"   "+hashCode());
+        logTaskname();
+    }
+
+    private void logTaskname(){
+        try {
+            ActivityInfo info=getPackageManager().getActivityInfo(getComponentName(), PackageManager.GET_META_DATA);
+            Log.d("AActivity", info.taskAffinity);
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
     }
 }
--- app/src/main/java/com/example/myfirstapp/jump/BActivity.java
+++ app/src/main/java/com/example/myfirstapp/jump/BActivity.java
@@ -2,7 +2,10 @@ package com.example.myfirstapp.jump;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -17,16 +20,29 @@ import com.example.myfirstapp.util.ToastUtil;
 public class BActivity extends AppCompatActivity {
     private TextView mTvtitle;
     private Button mBtnFinish;
+    private Button mBtnjumptoa;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_b);
+        Log.d("BActivity","---onCreate---");
+        Log.d("BActivity","taskid:"+getTaskId()+"   hash:"+hashCode());
+        logTaskname();
         mTvtitle=findViewById(R.id.tv_title_jump);
+        mBtnjumptoa=findViewById(R.id.btn_jumptoa);
         mBtnFinish=findViewById(R.id.btn_finish);
         final Bundle bundle=getIntent().getExtras();
         String name=bundle.getString("name");
         int number=bundle.getInt("number");
         mTvtitle.setText(name+","+number);
+
+        mBtnjumptoa.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(BActivity.this,AActivity.class);
+                startActivity(intent);
+            }
+        });
 
         mBtnFinish.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -43,4 +59,20 @@ public class BActivity extends AppCompatActivity {
 
     }
 
+    @Override
+    protected void onNewIntent(Intent intent) {
+        super.onNewIntent(intent);
+        Log.d("BActivity","---onNewIntent---");
+        Log.d("BActivity","taskid:"+getTaskId()+"   "+hashCode());
+        logTaskname();
+    }
+
+    private void logTaskname(){
+        try {
+            ActivityInfo info=getPackageManager().getActivityInfo(getComponentName(), PackageManager.GET_META_DATA);
+            Log.d("BActivity", info.taskAffinity);
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
 }
